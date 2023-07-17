bb26f7bf4264edaa324b7fefb267774014ddfb65
thanosronin51 <88163214+thanosronin51@users.noreply.github.com>
2023-07-17 06:10:33 +0100

    Update settings.py

--- bankingsystem/settings.py
+++ bankingsystem/settings.py
@@ -28,7 +28,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tempus_dominus',
     'crispy_forms',
     'crispy_bootstrap4',
  
