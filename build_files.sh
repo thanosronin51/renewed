e50b32e811b0b1f77d7d2660aae95a894f9d4839
thanosronin51 <88163214+thanosronin51@users.noreply.github.com>
2023-07-19 16:47:27 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,11 @@
 pip install django
 pip install celery
 pip install django-tempus-dominus
+pip install django-crispy-forms
+pip install django-environ
+pip install django-storages
+pip install crispy-heroku
+pip install django-timezone-field
+pip install django-user-agents
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
