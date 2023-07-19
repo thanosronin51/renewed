6ee570ebae6b4678569c6421e059e0f2f1966fc2
thanosronin51 <88163214+thanosronin51@users.noreply.github.com>
2023-07-19 21:58:36 +0100

    Update settings.py

--- bankingsystem/settings.py
+++ bankingsystem/settings.py
@@ -27,16 +27,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tempus_dominus',
-    'crispy_forms',
-    'crispy_bootstrap4',
+
  
     'accounts',
     'core',
     'transactions',
     'bankcard',
     'rest_framework',
-    'corsheaders',
     'storages'
 ]
 
@@ -153,15 +150,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = TIME_ZONE
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
-CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MESSAGE_TAGS = {
     messages.SUCCESS: 'alert-success',
