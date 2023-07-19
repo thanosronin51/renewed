981c03dc1b243f51877f80a007628d416be0a550
thanosronin51 <88163214+thanosronin51@users.noreply.github.com>
2023-07-19 22:03:14 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -4,5 +4,7 @@ pip install django-storages
 pip install django-timezone-field
 pip install django-user-agents
 pip install djangorestframework
+pip install psycopg2
+pip install psycopg2-binary
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
